aa908bd42df344c8c15b1a31f1900b777c09dfa4
Hrishikesh Barman <oss@geekodour.org>
2025-06-21 21:13:15 +0530

    wip

--- by-language/python/fast-api-pg/{{cookiecutter.project_slug}}/tests/test_sample.py
+++
@@ -1,5 +0,0 @@
-def test_sample_function(sample_fixture: str) -> None:
-    """A sample test function that uses a fixture."""
-    # S101: For simple equality checks, assert is idiomatic.
-    # For more complex scenarios, pytest's assertion helpers might be preferred.
-    assert sample_fixture == "sample_data"  # noqa: S101
