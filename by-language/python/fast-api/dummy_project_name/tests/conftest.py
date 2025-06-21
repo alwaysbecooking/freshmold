bf5a5167ce7c4c5937134543e58c30b8edbf7056
Hrishikesh Barman <oss@geekodour.org>
2025-06-21 07:21:40 +0530

    wip

---
+++ by-language/python/fast-api/dummy_project_name/tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+
+# tests/
+# ├── conftest.py               # db_session, http_client — global fixtures
+# ├── test_helpers/             # shared test utilities and mock data
+# ├── test_data/                # mock data etc.
+# ├── package_a/
+# │   ├── test_xyz.py
+# │   ├── conftest.py           # fixtures (package_a)
+# │   ├── test_helpers/         # shared test utilities and mock data (package_a)
+# │   └── test_data/            # mock data etc. (package_a)
+# ├── package_b/
+# │   ├── test_pqr.py
+# │   ├── conftest.py           # fixtures (package_b)
+# │   ├── test_helpers/         # shared test utilities and mock data (package b)
+# │   └── test_data/            # mock data etc. (package_b)
+
+
+@pytest.fixture
+def sample_fixture() -> str:
+    """Provide sample dummy data for tests."""
+    return "sample_data"
